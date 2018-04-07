eefbbea94162cd97a50a74378bb3d81b507b95d2
Benjamin Weidig <ben@netzgut.net>
2018-04-07 09:17:56 +0200

    Updated Tortuga (1.2.6)

--- Formula/tortuga.rb
+++ Formula/tortuga.rb
@@ -1,9 +1,9 @@
 class Tortuga < Formula
     desc "CLI tool for fetching/pushing/rebasing multiple git repositories at once"
     homepage "https://github.com/benweidig/tortuga"
-    url "https://github.com/benweidig/tortuga/releases/download/v1.2.5/tortuga-1.2.5_darwin_amd64.tar.gz"
-    sha256 "fc79579b062a157456f8fec60e8f2d22aa6ca32c9417b1bda8a03f7115be679f"
-    version "1.2.5"
+    url "https://github.com/benweidig/tortuga/releases/download/v1.2.6/tortuga-1.2.6_darwin_amd64.tar.gz"
+    sha256 "b77709aed20fa3d699d98402548d2584b30c114f08f136a6aaa36d618aa63465"
+    version "1.2.6"
 
     depends_on "git"
     def install
