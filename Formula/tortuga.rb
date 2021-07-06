21ff83adfb4b729112bcbb4812c2f0323c6cd48f
Ben Weidig <ben@netzgut.net>
2021-07-06 21:36:30 +0200

    tortuga: 2.2.4
    tortuga: 2.2.4

--- Formula/tortuga.rb
+++ Formula/tortuga.rb
@@ -1,11 +1,21 @@
 class Tortuga < Formula
     desc "CLI tool for fetching/pushing/rebasing multiple git repositories at once"
     homepage "https://github.com/benweidig/tortuga"
-    url "https://github.com/benweidig/tortuga/releases/download/v2.2.2/tortuga-2.2.2_darwin_amd64.tar.gz"
-    sha256 "037808c90af9458512a228105ea2188771b4d8c43b00fd791532692d478e7dfc"
-    version "2.2.2"
+    license "MIT"
+    version "2.2.4"
 
     depends_on "git"
+
+    if OS.mac?
+        if Hardware::CPU.intel?
+            url "https://github.com/benweidig/tortuga/releases/download/v2.2.4/tortuga-2.2.4_darwin_amd64.tar.gz"
+            sha256 "2a5e4503275f0c6f0991faf7ff6cb140564d49e5519dec524b9ce09eae4bcfb6"
+        else
+            url "https://github.com/benweidig/tortuga/releases/download/v2.2.4/tortuga-2.2.4_darwin_arm64.tar.gz"
+            sha256 "db35a3d2961e284cd709cb0bfd2d6138a042c5c5a6176b1d6a78d999fe2a2182"
+        end
+    end
+
     def install
         bin.install "tt"
     end
