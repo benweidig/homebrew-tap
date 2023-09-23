7bfc796a973a5d35cd0a9b5fe711a14a52291089
Ben Weidig <ben@netzgut.net>
2023-09-23 16:06:40 +0200

    tortuga: 2.3.4 (fix)

--- Formula/tortuga.rb
+++ Formula/tortuga.rb
@@ -11,7 +11,7 @@ class Tortuga < Formula
             url "https://github.com/benweidig/tortuga/releases/download/v2.3.4/tortuga-2.3.4_darwin_amd64.tar.gz"
             sha256 "2cf10ab161e5121f4a36d0a151d9b928fb38ec4c9206c05920cb3e7f4f1d9222"
         else
-            url "https://github.com/benweidig/tortuga/releases/download/v2.3.3/tortuga-2.3.3_darwin_arm64.tar.gz"
+            url "https://github.com/benweidig/tortuga/releases/download/v2.3.4/tortuga-2.3.4_darwin_arm64.tar.gz"
             sha256 "f1dca2aed68ab4f43849ea203e5bbc8d9f945abbcbe82b7c4389e31c32324f93"
         end
     end
