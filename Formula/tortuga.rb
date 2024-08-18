7f55a1da0820b1ef414fa3c8d55931874ab520ee
Ben Weidig <ben@netzgut.net>
2024-08-18 12:37:01 +0200

    tortuga: 2.3.5

--- Formula/tortuga.rb
+++ Formula/tortuga.rb
@@ -2,17 +2,17 @@ class Tortuga < Formula
     desc "CLI tool for fetching/pushing/rebasing multiple git repositories at once"
     homepage "https://github.com/benweidig/tortuga"
     license "MIT"
-    version "2.3.4"
+    version "2.3.5"
 
     depends_on "git"
 
     if OS.mac?
         if Hardware::CPU.intel?
-            url "https://github.com/benweidig/tortuga/releases/download/v2.3.4/tortuga-2.3.4_darwin_amd64.tar.gz"
-            sha256 "2cf10ab161e5121f4a36d0a151d9b928fb38ec4c9206c05920cb3e7f4f1d9222"
+            url "https://github.com/benweidig/tortuga/releases/download/v2.3.5/tortuga-2.3.5_darwin_amd64.tar.gz"
+            sha256 "4b2390ffa0ebc8deea65f67d29b09b9a3255a605aca9eedc8f9467403759389c"
         else
-            url "https://github.com/benweidig/tortuga/releases/download/v2.3.4/tortuga-2.3.4_darwin_arm64.tar.gz"
-            sha256 "f1dca2aed68ab4f43849ea203e5bbc8d9f945abbcbe82b7c4389e31c32324f93"
+            url "https://github.com/benweidig/tortuga/releases/download/v2.3.5/tortuga-2.3.5_darwin_arm64.tar.gz"
+            sha256 "ba0471ca3da23ee8164d32fb64124a2b5a16a23d47a4d1af1d68626fcbc08e8b"
         end
     end
 
